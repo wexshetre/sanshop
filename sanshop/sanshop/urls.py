7a98b21efaec0cb3310da48758d9f7073f77805f
wexshetre <mangawi@protonmail.com>
2021-03-27 07:24:07 +0430

    first run postgres + templates_dir + sandbox setting succeed

--- sanshop/sanshop/urls.py
+++ sanshop/sanshop/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.apps import apps
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
+    path('', include(apps.get_app_config('oscar').urls[0])),
 ]
